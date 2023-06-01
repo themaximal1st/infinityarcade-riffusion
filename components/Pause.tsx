104b91824cee58355a4543d502b795a3252f3eab
The Maximalist <hello@themaximalist.com>
2023-06-01 10:18:09 -0700

    added infinity arcade radio ui

--- components/Pause.tsx
+++ components/Pause.tsx
@@ -2,40 +2,43 @@ import { useEffect } from "react";
 import { FiPause, FiPlay } from "react-icons/fi";
 
 interface PauseProps {
-  paused: boolean;
-  setPaused: (value: boolean) => void;
+    paused: boolean;
+    setPaused: (value: boolean) => void;
 }
 
-export default function Pause({
-  paused,
-  setPaused,
-}: PauseProps) {
+export default function Pause({ paused, setPaused }: PauseProps) {
+    // Print the state into the console
+    useEffect(() => {
+        if (paused) {
+            console.log("Pause");
+        } else {
+            console.log("Play");
+        }
+    }, [paused]);
 
-  // Print the state into the console
-  useEffect(() => {
-    if (paused) {
-      console.log("Pause");
-    } else {
-      console.log("Play");
-    }
-  }, [paused]);
-
-  var classNameCondition = ""
-  if (paused) {
-    classNameCondition="animate-pulse fixed z-20 top-4 right-4 md:top-8 md:right-8 w-14 h-14 rounded-full drop-shadow-lg flex justify-center items-center text-white text-2xl bg-red-500 hover:bg-red-600 ring-4 ring-red-700 focus:outline-none hover:drop-shadow-2xl"
-  } else {
-    classNameCondition="fixed z-20 top-4 right-4 md:top-8 md:right-8 bg-slate-100 w-14 h-14 rounded-full drop-shadow-lg flex justify-center items-center text-sky-900 text-2xl hover:text-white hover:bg-sky-600 hover:drop-shadow-2xl"
-  }
-
-  return (
-    <>
-      <button
-        title="Pause"
-        className= {classNameCondition}
-        onClick={() => setPaused(!paused)}
-      >
-        {paused ? <FiPlay /> : <FiPause />}
-      </button>
-    </>
-  );
-};
+    return (
+        <>
+            <button
+                className="text-white w-6 h-6 inline-block"
+                onClick={() => setPaused(!paused)}
+            >
+                {paused && (
+                    <img
+                        className="w-full h-full"
+                        src="/radio-stop.png"
+                        alt="Radio Stop"
+                        id="icon-stop"
+                    />
+                )}
+                {!paused && (
+                    <img
+                        className="w-full h-full"
+                        src="/radio-play.png"
+                        alt="Radio Play"
+                        id="icon-play"
+                    />
+                )}
+            </button>
+        </>
+    );
+}
